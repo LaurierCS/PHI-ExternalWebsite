f5e85acafcd23da0dda77f28cb88f65881dc6001
Lily <dinhhue2002@gmail.com>
2021-04-03 09:11:47 -0400

    Added: FAQ and Where can I sign up to be a member.

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,18 +1,19 @@
 import React from "react";
 import history from "../browserHistory";
-import ExampleA from "./ExampleA";
 const AboutUs = () => {
     return (
         <React.Fragment>
-            <h1
+            {/* <h1
                 className="mattIsCool"
                 onClick={() => {
                     history.push("/bye");
                 }}
             >
                 AboutUs
-            </h1>
-            <ExampleA></ExampleA>>
+            </h1> */}
+            <h2 className ="FAQ">FAQ</h2>
+            <div className = "Member">Where can I sign up to be a memeber?</div>
+
         </React.Fragment>
     );
 };
