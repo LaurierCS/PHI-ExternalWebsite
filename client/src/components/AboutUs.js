f02dc8e44312213f0900a45053beb75ddd6f639a
Lovette Oyewole <lovette.oyewole@icloud.com>
2021-04-03 21:56:21 -0400

    Added answer section for FAQ

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -12,7 +12,15 @@ const AboutUs = () => {
                 AboutUs
             </h1> */}
             <h2 className ="FAQ">FAQ</h2>
-            <div className = "Member">Where can I sign up to be a memeber?</div>
+            <div className = "Question">Where can I sign up to be a member?
+            </div>
+            <div className = "Answer">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+            eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+            </div>
 
         </React.Fragment>
     );
